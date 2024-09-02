4be8d2ded3c13727d68a1e1be2e4eb17446495a6
BHAAGYA <bhagyashreedas2002@gmail.com>
2024-09-02 11:25:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ComponentsLifeCycle from './components/ComponentsLifeCycle';
+
+
+function App() {
+  
+  
+  return (
+    <>
+      <ComponentsLifeCycle/>
+    </>
+  );
+}
+
+export default App;
